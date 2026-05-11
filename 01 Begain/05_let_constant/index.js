2ae72d69206c14723779a424378e86ed839dc96b
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-05-11 12:46:28 +0600

    begin

---
+++ 01 Begain/05_let_constant/index.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+
+//var: Variable redeclared করা যায়।
+var fullname = "murshid Mahmud";
+var fullname = 'Mahin';
+
+console.log(fullname);
+
+
+//let: Variable value change করা যায়। But varible different হবে।
+
+let fullname2 = 'Mahmud';
+fullname3 = 'khadi';
+fullname5 = 'Monika';
+
+console.log(fullname5);
+
+
+
+//constant: variable valu change করা যায় না Fixed হয়ে যায়। But Variable different হবে।
+
+const fullname4 = 'Dhaka';
+fullname9 = 'district';
+
+console.log(fullname4);
+
---
+++ 01 Begain/06_dataType/index.js
@@ -0,0 +1,60 @@
+//String
+let color = "Red";
+let Name = "Murshid";
+
+console.log(color);
+console.log(Name);
+document.write(Name);
+
+
+//Number
+let int = 15;
+let float = 2.444;
+
+console.log(int);
+console.log(float);
+
+//BigInt
+let x = 12452154254221354221n;
+let y = BigInt(125451254125428545254254);
+
+console.log(x);
+console.log(y);
+
+
+//Boolean
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(b);
+
+//Ojbject
+const person = {firstName: "Murshid", lastName: "Mahmud"};
+
+console.log(person);
+
+
+//Array object
+const cars = ["Volvo", "BMW", "Roles_Royles"];
+
+console.log(cars);
+
+
+//Date Object
+const date = new Date("2026-05-11");
+
+console.log(date);
+
+//Undefined
+let c;
+let d;
+
+
+//NULL
+let e = null;
+let q = null;
+
+//Symbol
+const g = Symbol();
+const h = Symbol();
