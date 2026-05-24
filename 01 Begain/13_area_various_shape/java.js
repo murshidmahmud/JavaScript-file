109f89bd76a3246971c7e16b7f77bcbf86ac88f8
Murshid <murshidmahmud4@gmail.com>
2026-05-25 01:14:33 +0600

    begin 25-05-2026

---
+++ 01 Begain/13_area_various_shape/java.js
@@ -0,0 +1,15 @@
+//Area of Rectangle:
+
+let height = parseFloat(prompt("Enter the height: "));
+let weight = parseFloat(prompt("Enter the weight: "));
+
+let area = height * weight;
+
+console.log("Rectangle Area: ", area);
+
+
+//Area of Triangle:
+
+area = (height*weight)/2;
+
+console.log("Traiangle area: ", area);
---
+++ 01 Begain/13_area_various_shape/temp.js
@@ -0,0 +1,7 @@
+//Temperature :
+
+let far = parseFloat(prompt("Enter the number: °F"));
+
+let cels =(far-32) * (5/9);
+
+console.log("Celcios is: ", cels,"°C");
