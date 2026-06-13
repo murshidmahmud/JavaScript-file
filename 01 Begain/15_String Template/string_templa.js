af6ab9f71e48cc184ddcbe466e56698660aa30ad
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-06-13 12:27:12 +0600

    begin 13-06-2026

---
+++ 01 Begain/15_String Template/string_templa.js
@@ -0,0 +1,12 @@
+let text = `My name is Murshid`;
+
+console.log(text);
+
+
+let firstname = "Murshid";
+let lastname = "Mahmud";
+
+let text2 = `Welcome ${firstname} ${lastname}`;
+
+console.log(text2);
+
