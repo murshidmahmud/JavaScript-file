557964382a5c25b6abee4f8eaadeeb5f6a50d728
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-06-15 02:06:35 +0600

    begin

---
+++ 01 Begain/17_break_continue/break.js
@@ -0,0 +1,16 @@
+for(let i=1; i<=100; i++){
+    if(i==10){
+        break;
+    }
+    document.write(" "+ i);
+}
+document.write("end number");
+
+
+
+for(let i=0; i<200; i++){
+    if(i%2==0){break;}
+
+    document.write(" ", i);
+}
+document.write("End");
