bde685d0139d3a224cdba8842ea795e5a61524bd
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-07-07 13:28:51 +0600

    beginging

---
+++ 01 Begain/18_function/arrow_function.js
@@ -0,0 +1,26 @@
+//Arrow function:
+
+//with parantheses or first bracket() and parameter:
+const multiply = (a,b) => a+b;
+
+console.log(multiply(20,30));
+
+
+//without parantheses()
+const square = x => x*x;
+
+console.log(square(20))
+
+
+//with curle bracket{}
+let num = (a,b) => {a*b};         //Undefined
+
+console.log(num(30,30))
+
+
+//with return:
+// let num2 = (a,b) => return a+b;
+
+// console.log(num2(4,5))
+
+
