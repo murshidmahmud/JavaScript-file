20dac038f80268c31f463b4f95cf70806f3a1dd6
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-07-13 13:38:19 +0600

    learn

---
+++ 01 Begain/19_Array/1D_array.js
@@ -0,0 +1,33 @@
+document.write(`<h2 style = "color: #000000; background: #f8b200;border-radius:108px;padding: 4px; text-align: center;">${"1D Array"}</h2>`);
+
+
+//1D array: 
+
+let num1 = [10,2,20,30,40,50,10];
+document.write("<h2>",num1);
+
+// let num2 = new Array(10,20,30,40,50,60);
+// document.write(num2);
+
+// let num3 = [10,50,80,88,99,55];
+
+// // console.log(num3[0]);
+// // console.log(num3[1]);
+// // console.log(num3[2]);
+
+//For loop use: 
+
+let number1 = [2,3,4,5,4,3,2];
+
+for(let i=0; i<number1.length; i++){
+    document.write("<br/>",number1[i]);
+} 
+
+
+//For....of loop use:
+
+let number2 = [222,333,666,58,888,88852];
+
+for(let value of number2){
+    document.write("<br/>", value);
+}
---
+++ 01 Begain/19_Array/2D_array.js
@@ -0,0 +1,4 @@
+document.write(`<h2 style = "color: #fca400; background: #5709ff;border-radius:108px;padding: 4px; text-align: center;">${"2D Array"}</h2>`);
+
+//2D array: 
+
